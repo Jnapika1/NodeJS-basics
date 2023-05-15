83fbef6519a467f5b8b6a01562e99070f5fe638f
Jnapika1 <jnapika.aish@gmail.com>
2023-05-15 21:10:16 +0530

    student obj created

--- first-app.js
+++ first-app.js
@@ -2,8 +2,19 @@
 // const fs = require('fs');
 // fs.writeFileSync('hello.txt', 'Hello from node js');
 
-let a=9;
-const b=8;
+// let a=9;
+// const b=8;
 
-const prod=(x, y)=> x*y;
-console.log(prod(a, b));
+// const prod=(x, y)=> x*y;
+// console.log(prod(a, b));
+
+const student = {
+    name: 'Alex',
+    age: 18,
+    dept: 'Communications',
+    intro(){
+        console.log(`Hi, I am ${this.name} of age ${this.age}, studing in ${this.dept} department`);
+    }
+}
+
+student.intro();
