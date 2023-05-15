9ae9bb3d04b21c19c30e5dfb77db29a26fef4d64
Jnapika1 <jnapika.aish@gmail.com>
2023-05-15 21:01:20 +0530

    arrow func

--- first-app.js
+++ first-app.js
@@ -1,3 +1,9 @@
-console.log('Hello World');
+// console.log('Hello World');
 // const fs = require('fs');
 // fs.writeFileSync('hello.txt', 'Hello from node js');
+
+let a=9;
+const b=8;
+
+const prod=(x, y)=> x*y;
+console.log(prod(a, b));
