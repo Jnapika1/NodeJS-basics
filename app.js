70af77fd6a44e71378c2a787d9620811eff7eef1
Jnapika1 <jnapika.aish@gmail.com>
2023-05-16 15:25:23 +0530

    server request and response

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const http = require('http');
 
-const server = http.createServer(function(req, res){
-    console.log('Aishwarya');
+const server = http.createServer((req, res)=>{
+    // console.log('Aishwarya');
+    const url=req.url;
+    if(url==='/home'){
+        res.write('<html>');
+        res.write('<head><title>Welcome</title></head>');
+        res.write('<body><h1>Welcome Home<h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(url==='/about'){
+        res.write('<html>');
+        res.write('<head><title>Welcome</title></head>');
+        res.write('<body><h1>Welcome to About us page<h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(url==='/node'){
+        res.write('<html>');
+        res.write('<head><title>Welcome</title></head>');
+        res.write('<body><h1>Welcome to my Node js Project<h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    if(url==='/'){
+        res.write('<html>');
+    res.write('<head><title>Welcome</title></head>');
+    res.write('<body><h1>Hello to Node JS<h1></body>');
+    res.write('</html>');
+    res.end();
+    }
+    
 });
 
 server.listen(4000);
