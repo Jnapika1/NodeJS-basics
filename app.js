f7c51aea6755378a86e3ea5bed6b5de7ac3f3883
Jnapika1 <jnapika.aish@gmail.com>
2023-05-16 19:12:17 +0530

    clean code with file

--- app.js
+++ app.js
@@ -2,24 +2,19 @@ const http = require('http');
 const fs=require('fs'); 
 
 const server = http.createServer((req, res)=>{
-    // console.log('Aishwarya');
     const url=req.url;
-    const method=req.method;
-    
-    
+    const method=req.method;  
     if(url==='/'){
         fs.readFile('message.txt', (err, data)=>{
             const msg=Buffer.from(data).toString();
             console.log(msg);
-            
             res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write(`<body>${msg}</body>`);
-        res.write('<body><form action="/message" method="POST"><input tupe="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-        })
-        
+            res.write('<head><title>Enter message</title></head>');
+            res.write(`<body>${msg}</body>`);
+            res.write('<body><form action="/message" method="POST"><input tupe="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+            return res.end();       
+        })    
     }
     if(url==='/message' && method==='POST'){
         const body=[];
