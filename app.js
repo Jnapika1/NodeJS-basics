0ae9c476c611d66bf8717958c4be486a75eaf05b
Jnapika1 <jnapika.aish@gmail.com>
2023-05-17 18:41:22 +0530

    module exports

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
 const http = require('http');
-const fs=require('fs'); 
 
-const server = http.createServer((req, res)=>{
-    const url=req.url;
-    const method=req.method;  
-    if(url==='/'){
-        fs.readFile('message.txt', (err, data)=>{
-            const msg=Buffer.from(data).toString();
-            console.log(msg);
-            res.write('<html>');
-            res.write('<head><title>Enter message</title></head>');
-            res.write(`<body>${msg}</body>`);
-            res.write('<body><form action="/message" method="POST"><input tupe="text" name="message"><button type="submit">Send</button></form></body>');
-            res.write('</html>');
-            return res.end();       
-        })    
-    }
-    if(url==='/message' && method==='POST'){
-        const body=[];
-        req.on('data', (chunk)=>{
-            body.push(chunk);
-        })
-        return req.on('end', ()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const msg=parsedBody.split('=')[1];
-            fs.writeFile('message.txt', msg, (err)=>{
-                res.statusCode=302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        })
-        
-    }
-});
+const routes=require('./route');
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(4000);
---
+++ route.js
@@ -0,0 +1,47 @@
+const fs=require('fs'); 
+
+const requestHandler = (req, res)=>{
+    const url=req.url;
+    const method=req.method;
+
+if(url==='/'){
+    fs.readFile('message.txt', (err, data)=>{
+        const msg=Buffer.from(data).toString();
+        console.log(msg);
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write(`<body>${msg}</body>`);
+        res.write('<body><form action="/message" method="POST"><input tupe="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();       
+    })    
+}
+if(url==='/message' && method==='POST'){
+    const body=[];
+    req.on('data', (chunk)=>{
+        body.push(chunk);
+    })
+    return req.on('end', ()=>{
+        const parsedBody=Buffer.concat(body).toString();
+        const msg=parsedBody.split('=')[1];
+        fs.writeFile('message.txt', msg, (err)=>{
+            res.statusCode=302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
+    })
+    
+}
+}
+// module.exports = requestHandler;
+
+// module.exports={
+//     handler : requestHandler,
+//     someText : 'some hard code text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'some hard code text';
+
+exports.handler = requestHandler;
+exports.someText = 'some hard code text';
